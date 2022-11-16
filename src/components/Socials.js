63fc9b042517975d98d5e2150ad28f65ee40ae4a
twój login do GitHuba <96842769+MaksBernatowicz@users.noreply.github.com>
2022-11-16 12:28:17 +0100

    finished Header.js and Socials.js

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,27 @@
 import React from "react";
+// import icons
+import { ImFacebook, ImInstagram } from "react-icons/im";
 
 const Socials = () => {
-  return <div className="hidden xl:flex ml-24">Socials</div>;
+  return (
+    <div className="hidden xl:flex ml-24">
+      <ul className="flex gap-x-8 text-xl">
+        <li>
+          <a href="https://www.facebook.com/lukaszniemiecphoto" target="blank">
+            <ImFacebook />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/lukaszniemiec.photo"
+            target="blank"
+          >
+            <ImInstagram />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Socials;
